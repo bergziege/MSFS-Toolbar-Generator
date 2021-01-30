3a72367173a722390364c9d4627b1267169a6022
Bernd Boruttau <b.boruttau@web.de>
2021-01-30 20:32:16 +0100

    Added material design for xaml

--- src/MsfsToolbarGenerator/UI/Main/MainView.xaml.cs
+++ src/MsfsToolbarGenerator/UI/Main/MainView.xaml.cs
@@ -1,15 +1,22 @@
-﻿using System.Windows;
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Navigation;
 
-namespace De.Berndnet2000.MsfsToolbarGenerator.UI.Main
-{
+namespace De.Berndnet2000.MsfsToolbarGenerator.UI.Main {
     /// <summary>
-    /// Interaktionslogik für MainView.xaml
+    ///     Interaktionslogik für MainView.xaml
     /// </summary>
-    public partial class MainView : Window
-{
-    public MainView()
-    {
-        InitializeComponent();
+    public partial class MainView : Window {
+        public MainView() {
+            InitializeComponent();
+        }
+
+        private void HyperlinkOnRequestNavigate(object sender, RequestNavigateEventArgs e) {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) {
+                var url = e.Uri.ToString().Replace("&", "^&");
+                Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") {CreateNoWindow = true});
+            }
+        }
     }
-}
 }
