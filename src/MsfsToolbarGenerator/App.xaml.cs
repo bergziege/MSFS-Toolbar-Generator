912de671d63b0f87c99cca2054a2009d430ed4f8
Bernd Boruttau <b.boruttau@web.de>
2021-02-14 10:50:43 +0100

    added service collection #11

--- src/MsfsToolbarGenerator/App.xaml.cs
+++ src/MsfsToolbarGenerator/App.xaml.cs
@@ -1,22 +1,50 @@
+using System;
 using System.Windows;
+using De.Berndnet2000.MsfsToolbarGenerator.Services;
 using De.Berndnet2000.MsfsToolbarGenerator.Services.Impl;
 using De.Berndnet2000.MsfsToolbarGenerator.UI.FileSelect.ViewCommands;
 using De.Berndnet2000.MsfsToolbarGenerator.UI.FolderSelect.ViewCommands;
 using De.Berndnet2000.MsfsToolbarGenerator.UI.Main;
 using De.Berndnet2000.MsfsToolbarGenerator.UI.Main.ViewModels;
 using De.Berndnet2000.MsfsToolbarGenerator.Wrapper;
+using Microsoft.Extensions.DependencyInjection;
 
-namespace De.Berndnet2000.MsfsToolbarGenerator {
+namespace De.Berndnet2000.MsfsToolbarGenerator
+{
     /// <summary>
     ///     Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application {
-        protected override void OnStartup(StartupEventArgs e) {
+    public partial class App : Application
+    {
+        private IServiceProvider _serviceProvider;
+
+        public App()
+        {
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+
+            _serviceProvider = serviceCollection.BuildServiceProvider();
+        }
+
+        private void ConfigureServices(ServiceCollection serviceCollection)
+        {
+            serviceCollection.AddSingleton<ISelectFileViewCommand, SelectFileViewCommand>();
+            serviceCollection.AddSingleton<ISelectFolderViewCommand, SelectFolderViewCommand>();
+
+            serviceCollection.AddSingleton<IMainViewModel, MainViewModel>();
+
+            serviceCollection.AddSingleton<IFileSystem, SystemFileSystem>();
+
+            serviceCollection.AddSingleton<IBuildService, BuildService>();
+            serviceCollection.AddSingleton<ILayoutCreationService, LayoutCreationService>();
+            serviceCollection.AddSingleton<ITokenizer, Tokenizer>();
+            serviceCollection.AddSingleton<IToolbarCreationService, ToolbarCreationService>();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
             base.OnStartup(e);
-            IFileSystem fileSystem = new SystemFileSystem();
-            IMainViewModel vm = new MainViewModel(new SelectFolderViewCommand(),
-                new ToolbarCreationService(fileSystem, new Tokenizer()), new LayoutCreationService(fileSystem),
-                new BuildService(fileSystem), new SelectFileViewCommand());
+            IMainViewModel vm = _serviceProvider.GetService<IMainViewModel>();
             vm.LoadUserSettings();
             var view = new MainView();
             view.DataContext = vm;
